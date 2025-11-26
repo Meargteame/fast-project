f5bd697d098855941af6ab9331252c7323ebf9e0
meargteame <mearegteame99995555@gmail.com>
2025-11-26 15:44:34 +0300

    feat:api get all posts and get post with id apis done

--- app/app.py
+++ app/app.py
@@ -1,11 +1,37 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
 app = FastAPI()
 
 
 
-@app.get('/hello-world')
-def helloworld():
-    return {"message":"Hello World"}
+# @app.get('/hello-world')
+# def helloworld():
+#     return {"message":"Hello World"}
 
 
 
+text_posts = {
+     1:{"title":"New Post","content":"cool test post"},
+     2:{"title":"New Post","content":"cool test post"},
+     3:{"title":"New Post","content":"cool test post"},
+     4:{"title":"New Post","content":"cool test post"},
+     5:{"title":"New Post","content":"cool test post"},
+     6:{"title":"New Post","content":"cool test post"},
+     7:{"title":"New Post","content":"cool test post"},
+     8:{"title":"New Post","content":"cool test post"},
+
+}
+
+@app.get("/posts")
+def get_all_posts(limit:int=None):
+    if limit:
+        return list(text_posts.values())[:limit]
+    return text_posts
+
+
+
+# path parameters
+@app.get("/post/{id}")
+def get_post(id:int):
+    if id not in text_posts:
+        raise HTTPException(status_code=404,detail="Post not found")
+    return text_posts.get(id)
