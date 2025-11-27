96f1f2dd33c3eca418483b6f474c866e28dd9b6f
meargteame <mearegteame99995555@gmail.com>
2025-11-27 13:45:53 +0300

    create post api is done

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI,HTTPException
+from app.schemas import PostCreate
 app = FastAPI()
 
 
@@ -25,7 +26,7 @@ text_posts = {
 def get_all_posts(limit:int=None):
     if limit:
         return list(text_posts.values())[:limit]
-    return text_posts
+    return text_posts 
 
 
 
@@ -35,3 +36,15 @@ def get_post(id:int):
     if id not in text_posts:
         raise HTTPException(status_code=404,detail="Post not found")
     return text_posts.get(id)
+
+
+
+# request body // POST
+
+@app.post("/posts")
+def create_post(post:PostCreate):
+    new_post = {"title":post.title,"content":post.content}
+    text_posts[max(text_posts.keys)+1] = new_post
+    return new_post
+
+
